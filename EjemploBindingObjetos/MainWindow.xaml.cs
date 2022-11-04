caf0d9b4baf060d30e3b3312463fcae70c0a7c28
JOSÉ ALFREDO ZARAGOZA MARCO <joszarmar@alu.edu.gva.es>
2022-11-04 13:38:45 +0100

    Agregar archivos de proyecto.

---
+++ EjemploBindingObjetos/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using EjemploBindingObjetos.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EjemploBindingObjetos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Persona jose;
+        public MainWindow()
+        {
+            InitializeComponent();
+            jose = new Persona("Jose", 0);
+            Persona[] persona = new Persona[] {
+                new Persona("Jose", 20),
+                new Persona("Javi", 24),
+                new Persona("Ian", 21),
+                new Persona("Ruben", 20),
+                new Persona("Martina", 19),
+                new Persona("Pablo", 29)
+            };
+
+            Formulario.DataContext = jose;
+        }
+
+        private void CumpleañosButton_Click(object sender, RoutedEventArgs e)
+        {
+            ((Persona)Formulario.DataContext).Edad++;
+        }
+
+        private void SaberEdadButton_Click(object sender, RoutedEventArgs e)
+        {
+            jose = ((Persona)Formulario.DataContext);
+            MessageBox.Show("Edad: " + jose.Edad);
+        }
+    }
+}
---
+++ EjemploBindingObjetos/Modelo/Persona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploBindingObjetos.Modelo
+{
+    class Persona : INotifyPropertyChanged
+    {
+        private int? edad;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public int? Edad { 
+            get { return edad; }
+            
+            set 
+            {
+                if (this.edad != value)
+                {
+                    edad = value;
+                    this.NotifyPropertyChanged("Edad");
+                }
+            }
+        }
+
+        public string Nombre { get; set; }
+
+        public Persona() { }
+
+        public Persona(string nombre, int edad)
+        {
+            Edad = edad;
+            Nombre = nombre;
+        }
+        private void NotifyPropertyChanged(string v)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v));
+        }
+    }
+}
